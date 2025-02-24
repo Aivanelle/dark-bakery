5ac341dc65fc6d83156a6d116457b5bbea7944c1
Aivan <dominguezmiguel715@gmail.com>
2025-02-24 16:03:09 -0700

    Children rendering component updated.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,12 @@
-import { PropsWithChildren } from 'react'
+import { Outlet } from 'react-router'
 import { Footer } from './Footer'
 import { Header } from './Header'
 
-export const Layout = ({ children }: PropsWithChildren) => {
+export const Layout = () => {
   return (
     <>
       <Header />
-      {children}
+      <Outlet />
       <Footer />
     </>
   )
