9583fcf10ccc2edbebdc05725ff4a2d181e355b8
Aivan <dominguezmiguel715@gmail.com>
2025-02-24 16:10:31 -0700

    chore: Root Layout component folder updated.

--- src/components/layouts/RootLayout/index.tsx
+++ src/components/layouts/RootLayout/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router'
 import { Footer } from './Footer'
 import { Header } from './Header'
 
-export const Layout = () => {
+export const RootLayout = () => {
   return (
     <>
       <Header />
