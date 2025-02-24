7371320b70b9ed17881a6c3533bffe37fae78798
Aivan <dominguezmiguel715@gmail.com>
2025-02-24 15:58:08 -0700

    chore: Props updated.

--- src/components/ContainedIconButton.tsx
+++ src/components/ContainedIconButton.tsx
@@ -1,9 +1,12 @@
-import { Box, IconButton, IconButtonProps } from '@mui/material'
+import { Box, IconButton, IconButtonProps, IconButtonTypeMap } from '@mui/material'
 import { ElementType, PropsWithChildren } from 'react'
 
 type Props<T extends ElementType> = PropsWithChildren<IconButtonProps<T>> & { component?: T }
 
-export const ContainedIconButton = <T extends ElementType>({ children, ...rest }: Props<T>) => {
+export const ContainedIconButton = <T extends ElementType = IconButtonTypeMap['defaultComponent']>({
+  children,
+  ...rest
+}: Props<T>) => {
   return (
     <Box sx={[(theme) => ({ backgroundColor: theme.palette.primary.main }), { borderRadius: 999 }]}>
       <IconButton
