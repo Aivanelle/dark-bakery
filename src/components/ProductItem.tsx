dac54355a5ad87494d6b6ba2c74631d4dda9464b
Aivan <dominguezmiguel715@gmail.com>
2025-02-25 01:10:00 -0700

    Component added.

---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,61 @@
+import { FavoriteBorderOutlined, StarRounded } from '@mui/icons-material'
+import { Box, Button, ButtonBase, Paper, Stack } from '@mui/material'
+import productPlaceholder from '/product-placeholder.jpg'
+
+type Props = {
+  name: string
+  rating: number
+  price: number
+}
+
+export const ProductItem = ({ name, rating, price }: Props) => {
+  return (
+    <Paper
+      variant='outlined'
+      sx={{ borderRadius: 4, minHeight: 344, height: '100%', display: 'flex', flexDirection: 'column' }}
+    >
+      <Box className='relative rounded-t-2xl overflow-hidden'>
+        <ButtonBase className='w-full'>
+          <img
+            className='h-50 w-full object-cover'
+            src={productPlaceholder}
+          />
+        </ButtonBase>
+        <ButtonBase
+          sx={{ position: 'absolute', top: 16, right: 16, p: 1, backgroundColor: 'white', borderRadius: '50%' }}
+        >
+          <FavoriteBorderOutlined fontSize='small' />
+        </ButtonBase>
+      </Box>
+      <Stack
+        className='p-4 justify-between items-start'
+        direction='row'
+        spacing={4}
+      >
+        <span className='font-medium text-lg overflow-ellipsis overflow-hidden line-clamp-2'>{name}</span>
+        <Stack
+          className='pt-2'
+          direction='row'
+          spacing={1}
+        >
+          <StarRounded sx={{ fontSize: '0.875rem', color: 'orange' }} />
+          <span className='text-xs'>{rating}</span>
+        </Stack>
+      </Stack>
+      <Stack
+        className='p-4 pt-0 mt-auto justify-between items-center'
+        direction='row'
+        spacing={4}
+      >
+        <Button
+          variant='outlined'
+          color='secondary'
+          sx={{ fontSize: '0.75rem', py: 2, px: 6 }}
+        >
+          Agregar
+        </Button>
+        <span className='font-medium'>${price.toFixed(2)}</span>
+      </Stack>
+    </Paper>
+  )
+}
